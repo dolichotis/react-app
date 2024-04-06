48ee9ac495f6f9b361ad34af0ae389ebf875944e
dolichotis <60782357+dolichotis@users.noreply.github.com>
2024-04-06 13:42:47 +0300

    lala

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './src/components/app/app';
-import './src/style.css';
+import App from './components/app/app';
+import './style.css';
 
 const todoapp = ReactDOM.createRoot(document.getElementById('todoapp'));
 todoapp.render(<App />);
